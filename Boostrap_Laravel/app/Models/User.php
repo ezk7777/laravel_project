c4bef77dff8d8cd2d9a16ac6a28904f219db0b13
Ezekiel <ezekiel.agbogah@epitech.eu>
2026-04-24 11:36:02 +0000

    [ADD] Configuration et création de mes différents controllers

--- Boostrap_Laravel/app/Models/User.php
+++ Boostrap_Laravel/app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -44,5 +45,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function articles(){
+        return $this->hasMany(Article::class);
     }
 }
